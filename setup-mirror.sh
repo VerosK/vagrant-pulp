02a47a25d00f03b55b9428597467f3a7d49ec524
Veros Kaplan <veros@inuits.eu>
2015-07-12 22:43:31 +0200

    Added some notes to setup script

--- setup-mirror.sh
+++ setup-mirror.sh
@@ -19,3 +19,12 @@ pulp-admin rpm repo uploads rpm  --repo-id=foreman -f /tmp/foreman-release.rpm
 # Possibley add feed
 
 #pulp-admin rpm repo update --repo=foreman --feed="http://yum.theforeman.org/releases/1.7/el7/x86_64"
+
+# run feed sync automatically
+#pulp-admin rpm repo sync run --repo=foreman
+
+# synchronize every 1 hour
+#pulp-admin rpm repo sync schedules create --repo=foreman --schedule PT1H
+
+# synchronize every 12HOURS
+#pulp-admin rpm repo sync schedules create --repo=foreman --schedule PT12H
